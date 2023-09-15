588171f6c77e63a86fad4b37fe485fef1da1e16d
AWTMODS <aadithaadith14@gmail.com>
2023-09-15 21:08:05 +0530

    kk

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php
 
+
+
+
 include("connection.php");
-include("indexphp.php");
+if(isset($_post['submit'])){
+
+
+      $email = $_post['email'];
+      $password = $_post['password'];
+
+
+      $sql = "SELECT * FROM details WHERE email = '$email' and password='$password";
+      $result=mysqli_query($conn,$sql);
+      $num = mysqli_num_rows($result);
+
+
+
+      if($num > 0){
+
+            header("location:welcome.php");
+
+            echo'<script>alert("Email Already Existed")<\script>';
+      }
+
+      else{
+
+
+           echo'<script>alert("Invaild email / password")<\script>';
+      }
+
+}
+
+
 ?>
 
 
